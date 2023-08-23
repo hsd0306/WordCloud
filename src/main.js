0013af8f5fd9795835401d3b9b1a56d32461f278
hsd0306 <sdhong@unist.ac.kr>
2023-08-23 13:06:17 +0900

    Add Navigation Bar

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDom.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
